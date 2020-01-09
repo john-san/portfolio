c632e9b2b45f4086051d8744e1c5023e32be95f2
John Nguyen <nguyen.johnsan@gmail.com>
2020-01-09 07:51:24 -0800

    add social icons includes and animateCSS

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -4,3 +4,26 @@ $(document).foundation()
 $('.reveal').on('click', (e) => {
   $('.reveal').foundation('close');
 });
+
+// modified animateCSS helper, from https://github.com/daneden/animate.css
+function animateCSS(element, animationName, callback) {
+  const node = element;
+  node.classList.add('animated', animationName);
+
+  function handleAnimationEnd() {
+    node.classList.remove('animated', animationName);
+    node.removeEventListener('animationend', handleAnimationEnd);
+
+    if (typeof callback === 'function') callback()
+  }
+
+  node.addEventListener('animationend', handleAnimationEnd);
+};
+
+
+$(document).ready(() => {
+  const portfolioCells = [...document.querySelectorAll('.portfolio-index .cell')];
+  portfolioCells.forEach((cell) => {
+    animateCSS(cell, 'fadeInLeft');
+  });
+});
