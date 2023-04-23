3696b6ab9b91aa19dc5fc0b14a820666de58109b
John Nguyen <npjohn92@gmail.com>
2023-04-23 05:30:45 -0700

    add tailwind and start nav

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,5 +1,59 @@
+'use client'
 import React from 'react'
-
+import Link from 'next/link'
 export default function NavBar() {
-	return <div>NavBar</div>
+	const [isOpen, setIsOpen] = React.useState(false)
+
+	const clickHandler = () => {
+		setIsOpen(!isOpen)
+	}
+
+	return (
+		<nav className='flex items-center justify-between flex-wrap bg-slate-600 p-6'>
+			<div className='flex items-center flex-shrink-0 text-white mr-6'>
+				<span className='font-bold text-xl tracking-tight'>
+					<Link href='/'>John-san's Portfolio</Link>
+				</span>
+			</div>
+
+			{/* Hamburger Icon */}
+			<div className='block lg:hidden'>
+				<button
+					className='flex items-center px-3 py-2 border rounded text-white/90 border-white/90 hover:text-white hover:border-white'
+					onClick={clickHandler}
+				>
+					<svg
+						className='fill-current h-3 w-3'
+						viewBox='0 0 20 20'
+						xmlns='http://www.w3.org/2000/svg'
+					>
+						<title>Menu</title>
+						<path d='M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z' />
+					</svg>
+				</button>
+			</div>
+			<div className='w-full block flex-grow lg:flex lg:items-center lg:w-auto'>
+				<div className='text-sm lg:flex-grow'>
+					<Link
+						href='/'
+						className='block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4'
+					>
+						Home
+					</Link>
+					<Link
+						href='/about'
+						className='block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4'
+					>
+						About
+					</Link>
+					<Link
+						href='/'
+						className='block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white'
+					>
+						Blog
+					</Link>
+				</div>
+			</div>
+		</nav>
+	)
 }
