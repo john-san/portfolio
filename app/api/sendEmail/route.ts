e3af86dd1e149ff388a6fa8ec499e645fec8a109
John Nguyen <npjohn92@gmail.com>
2024-03-30 17:53:22 -0700

    add nodemailer

---
+++ app/api/sendEmail/route.ts
@@ -0,0 +1,53 @@
+import { NextRequest } from 'next/server';
+import nodemailer from 'nodemailer';
+
+export async function POST(req: NextRequest): Promise<Response> {
+  if (req.method !== 'POST') {
+    return new Response('Method Not Allowed', { status: 405 });
+  }
+  const { name, email, message } = await req.json();
+
+  const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.GMAIL_EMAIL_ADDRESS,
+      pass: process.env.GMAIL_APP_PASSWORD,
+    },
+
+  });
+
+  try {
+    await transporter.sendMail({
+      // from: email, 
+      replyTo: email,
+      to: process.env.GMAIL_EMAIL_ADDRESS, 
+      subject: `New Contact Form Submission from ${name}`,
+      html: `<p>You have a new contact form submission:</p>
+      <p><strong>Name:</strong> ${name}</p>
+      <p><strong>Email:</strong> ${email}</p>
+      <p><strong>Message:</strong> ${message}</p>`,
+    });
+
+    return new Response('Email sent successfully', { status: 200 });
+  } catch (error) {
+    console.error("Failed to send email", error);
+    return new Response('Failed to send email', { status: 500 });
+  }
+};
+
+
+export async function GET(request: Request) {}
+ 
+export async function HEAD(request: Request) {}
+ 
+export async function PUT(request: Request) {}
+ 
+export async function DELETE(request: Request) {}
+ 
+export async function PATCH(request: Request) {}
+ 
+// If `OPTIONS` is not defined, Next.js will automatically implement `OPTIONS` and  set the appropriate Response `Allow` header depending on the other methods defined in the route handler.
+export async function OPTIONS(request: Request) {}
--- app/components/ContactForm.tsx
+++ app/components/ContactForm.tsx
@@ -1,80 +1,112 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser, faEnvelope, faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+	faUser,
+	faEnvelope,
+	faPaperPlane,
+} from "@fortawesome/free-solid-svg-icons"
+import { useState } from "react"
 
 export default function ContactForm() {
-  const handleSubmit = async (event: any) => {
-    event.preventDefault();
+	const [name, setName] = useState<string>("")
+	const [email, setEmail] = useState<string>("")
+	const [message, setMessage] = useState<string>("")
 
-    // Form validation logic can be more complex here
-    if (!event.target.email.value || !event.target.name.value || !event.target.message.value) {
-      alert('Please fill in all fields.');
-      return;
-    }
+	const handleSubmit = async (event: any) => {
+		event.preventDefault()
 
-    const formData = {
-      name: event.target.name.value,
-      email: event.target.email.value,
-      message: event.target.message.value,
-    };
+		if (!name || !email || !message) {
+			alert("Please fill in all fields.")
+			return
+		}
 
-    // Send the form data to our API route
-    // Same fetch logic as before
-  };
+		const formData = {
+			name,
+			email,
+			message,
+		}
 
-  return (
-    <section className='min-h-screen'>
-    <form onSubmit={handleSubmit} className="space-y-4 max-w-md mx-auto my-10">
-      <div>
-        <label htmlFor="name" className="flex items-center space-x-2">
-          <FontAwesomeIcon icon={faUser} />
-          <span>Name</span>
-        </label>
-        <input
-          id="name"
-          name="name"
-          type="text"
-          autoComplete="name"
-          required
-          className="mt-1 block w-full p-2 border-gray-300 shadow-sm rounded-md"
-          placeholder="Your name"
-        />
-      </div>
-      <div>
-        <label htmlFor="email" className="flex items-center space-x-2">
-          <FontAwesomeIcon icon={faEnvelope} />
-          <span>Email</span>
-        </label>
-        <input
-          id="email"
-          name="email"
-          type="email"
-          autoComplete="email"
-          required
-          className="mt-1 block w-full p-2 border-gray-300 shadow-sm rounded-md"
-          placeholder="Your email"
-        />
-      </div>
-      <div>
-        <label htmlFor="message" className="flex items-center space-x-2">
-          <FontAwesomeIcon icon={faPaperPlane} />
-          <span>Message</span>
-        </label>
-        <textarea
-          id="message"
-          name="message"
-          rows={4}
-          required
-          className="mt-1 block w-full p-2 border-gray-300 shadow-sm rounded-md"
-          placeholder="Your message"
-        ></textarea>
-      </div>
-      <button
-        type="submit"
-        className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-500 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-      >
-        Send
-      </button>
-    </form>
-    </section>
-  );
+		// Send the form data to our API route
+		const response = await fetch("/api/sendEmail", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(formData),
+		})
+
+		if (response.ok) {
+			console.log("Email sent successfully")
+      setName("");
+      setEmail("");
+      setMessage("");
+		} else {
+			console.log("Failed to send email")
+		}
+	}
+
+	return (
+		<section className="min-h-screen">
+			<form
+				onSubmit={handleSubmit}
+				className="space-y-4 max-w-md mx-auto my-10"
+			>
+				<div>
+					<label htmlFor="name" className="flex items-center space-x-2">
+						<FontAwesomeIcon icon={faUser} />
+						<span>Name</span>
+					</label>
+					<input
+						id="name"
+						name="name"
+						type="text"
+						autoComplete="name"
+						required
+						className="mt-1 block w-full p-2 border-gray-300 shadow-sm rounded-md"
+						placeholder="Your name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+					/>
+				</div>
+				<div>
+					<label htmlFor="email" className="flex items-center space-x-2">
+						<FontAwesomeIcon icon={faEnvelope} />
+						<span>Email</span>
+					</label>
+					<input
+						id="email"
+						name="email"
+						type="email"
+						autoComplete="email"
+						required
+						className="mt-1 block w-full p-2 border-gray-300 shadow-sm rounded-md"
+						placeholder="Your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+					/>
+				</div>
+				<div>
+					<label htmlFor="message" className="flex items-center space-x-2">
+						<FontAwesomeIcon icon={faPaperPlane} />
+						<span>Message</span>
+					</label>
+					<textarea
+						id="message"
+						name="message"
+						rows={4}
+						required
+						className="mt-1 block w-full p-2 border-gray-300 shadow-sm rounded-md"
+						placeholder="Your message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+					></textarea>
+				</div>
+				<button
+					type="submit"
+					className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-500 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+				>
+					Send
+				</button>
+			</form>
+		</section>
+	)
 }
